6d5c88c359845ebb53e3e4e3b322544930dc5342
rachit89 <rachit.maheshwari@squareops.com>
2024-03-26 17:30:37 +0530

    Upgraded Helm chart version of postgresql & prometheus-postgres-exporter

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -1,7 +1,7 @@
 locals {
   name        = "postgresql"
-  region      = "us-east-2"
-  environment = "prodd"
+  region      = "us-west-2"
+  environment = "prod"
   additional_tags = {
     Owner      = "organization_name"
     Expires    = "Never"
@@ -16,17 +16,17 @@ locals {
 }
 
 module "aws" {
-  source                           = "git@github.com:sq-ia/terraform-kubernetes-postgresql.git//modules/resources/aws"
+  source                           = "../../../modules/resources/aws"
   name                             = local.name
   environment                      = local.environment
-  cluster_name                     = ""
+  cluster_name                     = "prodd-eks"
   store_password_to_secret_manager = local.store_password_to_secret_manager
   custom_credentials_enabled       = local.custom_credentials_enabled
   custom_credentials_config        = local.custom_credentials_config
 }
 
 module "postgresql" {
-  source                      = "git@github.com:sq-ia/terraform-kubernetes-postgresql.git"
+  source                      = "../../../"
   postgresql_exporter_enabled = true
   custom_credentials_enabled  = local.custom_credentials_enabled
   custom_credentials_config   = local.custom_credentials_config
@@ -52,6 +52,6 @@ module "postgresql" {
   postgresql_restore_config = {
     bucket_uri       = "s3://backup-309017165673/pgdump__20231208095502.zip"
     file_name        = "pgdump__20231208095502.zip"
-    s3_bucket_region = "us-east-2"
+    s3_bucket_region = "us-west-2"
   }
 }
